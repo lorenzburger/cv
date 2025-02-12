8e9d3c0b9e0233df3fd8174411a5ae54f99c2704
lorenzburger <lorenzburger@arcro.de>
2025-02-12 10:06:25 +0100

    changed quotes

--- src/assets/quotes.js
+++ src/assets/quotes.js
@@ -2,18 +2,24 @@ export const quotes = [
   {
     "Leslie Nielsen": "“Doing nothing is very hard to do... you never know when you're finished.”",
   }, {
-    "Anthony Liccione": "“They say, timing is everything. But then they say, there is never a perfect time for anything.”",
-  }, {
     "Debasish Mridha": "“Sometimes the most important thing to do is to do nothing.”",
   }, {
     "Agatha Christie": "“I don't think necessity is the mother of invention. Invention arises directly from idleness, possibly also from laziness. To save oneself trouble.”",
   }, {
     "Ronald Reagan": "“I've heard that hard work never killed anyone, but I say why take the chance?”",
   }, {
-    "Gloria Pitzer": "“Procrastination is my sin. It brings me naught but sorrow. I know that I should stop it. In fact, I will, tomorrow”",
+    "Gloria Pitzer": "“Procrastination is my sin. It brings me naught but sorrow. I know that I should stop it. In fact, I will, tomorrow.”",
   }, {
     "Patrick Bennett": "“Laziness is the first step towards efficiency.”",
   }, {
     "Robert A. Heinlein": "“Progress is made by lazy men looking for easier ways to do things.”",
-  },
+  }, {
+    "Jules Renard": "“Laziness is nothing more than the habit of resting before you get tired.”",
+  }, {
+    "Mark Twain": "“Never put off till tomorrow what you can do the day after tomorrow just as well.”",
+  }, {
+    "Oscar Wilde": "“To do nothing at all is the most difficult thing in the world, the most difficult and the most intellectual.”",
+  }, {
+    "Arthur Bloch": "“A conclusion is the place where you got tired of thinking.”",
+  }
 ];
